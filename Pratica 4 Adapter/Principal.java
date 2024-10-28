ef5ec9cd75971948ab5d3d106a81c82229a732d9
MatheusLopes <120064706+MatheusLopes21@users.noreply.github.com>
2024-10-27 21:55:51 -0300

    Update Principal.java

--- Pratica 4 Adapter/Principal.java
+++ Pratica 4 Adapter/Principal.java
@@ -1,3 +1,7 @@
+/* Pratica 4 Adapter Method
+    Dupla: Igor Carvalho Braz -- RA: 42021158
+            Matheus Magalhães Alves Lopes -- RA: 4231922738*/ 
+
 public class Principal {
     public static void main(String[] args) {
         BuracoRedondo buraco = new BuracoRedondo(5);
